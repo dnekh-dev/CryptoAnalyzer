74be1b131a20f1137a75343003c8a94a2e818434
d_nekh <dnekh.dev@gmail.com>
2024-08-01 00:25:56 +0900

    Initial commit

---
+++ src/main/java/ru/javarush/dnekh/cryptoanalyzer/CryptoAnalyzer.java
@@ -0,0 +1,8 @@
+package ru.javarush.dnekh.cryptoanalyzer;
+
+public class CryptoAnalyzer {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Crypto-analyzer!");
+    }
+}
