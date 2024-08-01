753046b1c2b8847c45a2763594fbe9ab33d470cb
d_nekh <dnekh.dev@gmail.com>
2024-08-01 15:01:27 +0900

    Implemented CaesarCipher class. Also JavaDoc comments

--- src/main/java/ru/javarush/dnekh/cryptoanalyzer/model/CaesarCipher.java
+++ src/main/java/ru/javarush/dnekh/cryptoanalyzer/model/CaesarCipher.java
@@ -1,4 +1,71 @@
 package ru.javarush.dnekh.cryptoanalyzer.model;
 
+/**
+ * This class provides methods for encrypting and decrypting text using the Caesar cipher.
+ */
 public class CaesarCipher {
+
+    private final Alphabet alphabet;
+
+    /**
+     * Constructor initializes the CaesarCipher with a given Alphabet.
+     */
+    public CaesarCipher() {
+        this.alphabet = new Alphabet();
+    }
+
+    /**
+     * Encrypts a given text using the Caesar cipher with the specified key.
+     *
+     * @param text the text to encrypt
+     * @param key  the encryption key (shift value)
+     * @return the encrypted text
+     */
+    public String encrypt(String text, int key) {
+        StringBuilder encryptedText = new StringBuilder();
+        char[] alphabetArray = alphabet.getAlphabet();
+        int alphabetSize = alphabetArray.length;
+
+        for (char c : text.toCharArray()) {
+            if (alphabet.isCharInAlphabet(c)) {
+                int originalIndex = findCharIndex(c, alphabetArray);
+                int newIndex = (originalIndex + key) % alphabetSize;
+                if (newIndex < 0) {
+                    newIndex += alphabetSize;
+                }
+                encryptedText.append(alphabetArray[newIndex]);
+            } else {
+                encryptedText.append(c); // Keep characters not in the alphabet unchanged
+            }
+        }
+
+        return encryptedText.toString();
+    }
+
+    /**
+     * Decrypts a given text using the Caesar cipher with the specified key.
+     *
+     * @param text the text to decrypt
+     * @param key  the decryption key (shift value)
+     * @return the decrypted text
+     */
+    public String decrypt(String text, int key) {
+        return encrypt(text, -key);
+    }
+
+    /**
+     * Finds the index of a character in the alphabet array.
+     *
+     * @param c             the character to find
+     * @param alphabetArray the alphabet array
+     * @return the index of the character, or -1 if not found
+     */
+    private int findCharIndex(char c, char[] alphabetArray) {
+        for (int i = 0; i < alphabetArray.length; i++) {
+            if (alphabetArray[i] == c) {
+                return i;
+            }
+        }
+        return -1;
+    }
 }
