bdae7758f9c68aa42ecf4797fac60ac67893844a
d_nekh <dnekh.dev@gmail.com>
2024-08-03 14:47:01 +0900

    First implementation of FileHandler class: added readFile and writeFile methods

--- src/main/java/ru/javarush/dnekh/cryptoanalyzer/Main.java
+++ src/main/java/ru/javarush/dnekh/cryptoanalyzer/Main.java
@@ -1,8 +1,39 @@
 package ru.javarush.dnekh.cryptoanalyzer;
 
+import ru.javarush.dnekh.cryptoanalyzer.io.FileHandler;
+import ru.javarush.dnekh.cryptoanalyzer.model.CaesarCipher;
+
+import java.io.IOException;
+
 public class Main {
 
     public static void main(String[] args) {
 
+        CaesarCipher cipher = new CaesarCipher();
+
+        String text = "Привет, медвед!";
+        int key = 5;
+
+        String encryptedText = cipher.encrypt(text.toLowerCase(), key);
+        System.out.println("Encrypted: " + encryptedText);
+
+        String decryptedText = cipher.decrypt(encryptedText, key);
+        System.out.println("Decrypted: " + decryptedText);
+
+        System.out.println("-------------");
+
+        String pathToFile = "src/test.txt";
+        FileHandler fileHandler = new FileHandler();
+        try {
+            System.out.println(fileHandler.readFile(pathToFile));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        try {
+            fileHandler.writeFile(pathToFile, "Hello Kitty Super Mors!");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
--- src/main/java/ru/javarush/dnekh/cryptoanalyzer/io/FileHandler.java
+++ src/main/java/ru/javarush/dnekh/cryptoanalyzer/io/FileHandler.java
@@ -1,4 +1,35 @@
 package ru.javarush.dnekh.cryptoanalyzer.io;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+
+/**
+ * This class provides methods for reading text from a file and writing text to a file.
+ */
 public class FileHandler {
+
+    /**
+     * Reads the entire text content from the specified file.
+     *
+     * @param filePath the path to the file
+     * @return the text content of the file
+     * @throws IOException if an I/O error occurs reading from the file
+     */
+    public String readFile(String filePath) throws IOException {
+        return new String(Files.readAllBytes(Paths.get(filePath)));
+    }
+
+    /**
+     * Writes the specified text to the specified file.
+     * If the file already exists, it will be overwritten.
+     *
+     * @param filePath the path to the file
+     * @param content  the text content to write to the file
+     * @throws IOException if an I/O error occurs writing to the file
+     */
+    public void writeFile(String filePath, String content) throws IOException {
+        Files.write(Paths.get(filePath), content.getBytes(), StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING);
+    }
 }
